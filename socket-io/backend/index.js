d8d433c2b5d83fc5c64e7f31faf7b7b8be9d6c02
sarthak-gc <sarthakgc.np@gmail.com>
2025-03-19 21:19:06 +0545

    clean-up

--- socket-io/backend/index.js
+++ socket-io/backend/index.js
@@ -11,7 +11,6 @@ let users = [];
 io.on("connection", (socket) => {
   let storedSocketId = socket.handshake.query.id;
   if (!storedSocketId || storedSocketId == "undefined") {
-    console.log("HI there");
     storedSocketId = socket.id;
   }
   console.log(`User ${storedSocketId} connected`);
@@ -20,29 +19,22 @@ io.on("connection", (socket) => {
   if (!user) {
     user = {
       userId: storedSocketId,
-      messages: [],
     };
     users.push(user);
   }
-  socket.emit("previousMessages", user.messages);
 
   socket.on("messageToServer", (message) => {
-    user.messages.push(message);
-    console.log(user);
-    const time = Date.now();
-    io.emit("messageToClient", user.userId, message, time);
+    io.emit("messageToClient", user.userId, message);
   });
 
   socket.on("typing", () => {
-    console.log(socket);
     socket.broadcast.emit("user typing", "typing...");
+  });
+  socket.on("stopped typing", () => {
+    socket.broadcast.emit("user stopped typing");
   });
   socket.on("disconnect", () => {
     users = users.filter((user) => user.userId !== storedSocketId);
     console.log(`User ${storedSocketId} disconnected`);
   });
-});
-
-io.on("reconnect", () => {
-  console.log("reconnected");
 });
